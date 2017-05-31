25d7132403903560d6bdf8a5bd263e0b080ecca8
Robert P. Goldman <rpgoldman@sift.net>
2017-05-31 13:43:54 -0500

    Make start.lisp use ZOTDIR env. variable
    Allows zot to be arbitrarily placed without editing the lisp script.
    Defaults to original behavior.

--- bin/start.lisp
+++ bin/start.lisp
@@ -1,4 +1,9 @@
 #-:asdf (require :asdf)
-(pushnew "/usr/local/zot/asdfs/" asdf:*central-registry* :test #'equal)
+(let ((zotdir (uiop:getenv "ZOTDIR")))
+  (unless zotdir
+    (setf zotdir "/usr/local/zot"))
+  ;; FIXME: change to use new configuration language...
+  (pushnew (concatenate 'string zotdir "/asdfs/")
+           asdf:*central-registry* :test #'equal))
 
 
